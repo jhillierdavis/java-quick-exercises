00d8cb8976e7315e3b65d12a4788779259d50086
jhillierdavis <jhillierdavis@users.noreply.github.com>
2018-06-08 15:29:51 +0100

    Initial code.

---
+++ src/main/java/com/jhdit/algorithms/MostFrequentNumber.java
@@ -0,0 +1,7 @@
+package com.jhdit.algorithms;
+
+import java.util.List;
+
+public interface MostFrequentNumber {
+    Integer mostCommonNumber(List<Integer> numbers);
+}
---
+++ src/main/java/com/jhdit/algorithms/MostFrequentNumberImplementation.java
@@ -0,0 +1,39 @@
+package com.jhdit.algorithms;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given a list of numbers return the most frequently occurring number
+ */
+
+public class MostFrequentNumberImplementation implements MostFrequentNumber {
+    public Integer mostCommonNumber(List<Integer> numbers) {
+
+        Map<Integer, Integer> map = new HashMap();
+        for(Integer number: numbers) {
+            if (map.containsKey(number))    {
+                int currentValue = map.get(number);
+                map.put(number, ++currentValue);
+            } else {
+                map.put(number, 1);
+            }
+
+        }
+
+        int mostFrequentKey = -1;
+        int mostFrequentValue = -1;
+
+        for(Integer key: map.keySet())  {
+            int value = map.get(key);
+            if (value > mostFrequentValue)  {
+                mostFrequentKey = key;
+                mostFrequentValue = value;
+
+            }
+        }
+
+        return mostFrequentKey;
+    }
+}
