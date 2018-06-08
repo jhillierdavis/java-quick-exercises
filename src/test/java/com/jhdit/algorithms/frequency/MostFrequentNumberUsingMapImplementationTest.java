6cdb0fc2c3a04e7ef3964f9c23e4d973ac7b9e6a
jhillierdavis <jhillierdavis@users.noreply.github.com>
2018-06-08 17:20:15 +0100

    Refactored to simplify (removed verbose elements e.g. use of public, explicit Assertions reference).

--- src/test/java/com/jhdit/algorithms/frequency/MostFrequentNumberUsingMapImplementationTest.java
+++ src/test/java/com/jhdit/algorithms/frequency/MostFrequentNumberUsingMapImplementationTest.java
@@ -7,27 +7,27 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
-public class MostFrequentNumberUsingMapImplementationTest {
+class MostFrequentNumberUsingMapImplementationTest {
     // Subject under test (SUT)
-    MostFrequentNumber sut = new MostFrequentNumberUsingMapImplementation();
+    private MostFrequentNumber sut = new MostFrequentNumberUsingMapImplementation();
 
     @Test
-    public void testWithoutResult() {
+    void testWithoutResult() {
         // Given: an empty list
-        List<Integer> setOfNumbers = new ArrayList();
+        List<Integer> setOfNumbers = new ArrayList<>();
 
         // When
         Integer mostCommon = sut.mostCommonNumber(setOfNumbers);
 
         // Then: -1 is returned
-        Assertions.assertEquals(new Integer(-1), mostCommon);
+        assertEquals(new Integer(-1), mostCommon);
     }
 
     @DisplayName("unique match expected")
@@ -37,7 +37,7 @@ public class MostFrequentNumberUsingMapImplementationTest {
             "'1,2,2,3,3,3',3",
             "'99, 27, 99, 50',99"
     })
-    public void testWithUniqueResult(String inputNumbers, Integer expectedMostCommon) {
+    void testWithUniqueResult(String inputNumbers, Integer expectedMostCommon) {
         // Given
         List<Integer> numberList = toIntegerList(inputNumbers);
 
@@ -45,7 +45,7 @@ public class MostFrequentNumberUsingMapImplementationTest {
         Integer mostCommon = sut.mostCommonNumber(numberList);
 
         // Then
-        Assertions.assertEquals(expectedMostCommon, mostCommon);
+        assertEquals(expectedMostCommon, mostCommon);
     }
 
 
@@ -55,7 +55,7 @@ public class MostFrequentNumberUsingMapImplementationTest {
             "'1,1,1,2,2,2',1",
             "'7,5,5,7',5"
     })
-    public void testWithMultipleResults(String inputNumbers, Integer expectedMostCommon) {
+    void testWithMultipleResults(String inputNumbers, Integer expectedMostCommon) {
         // Given: a list of numbers where multiple are the most frequent
         List<Integer> numberList = toIntegerList(inputNumbers);
 
@@ -63,11 +63,11 @@ public class MostFrequentNumberUsingMapImplementationTest {
         Integer mostCommon = sut.mostCommonNumber(numberList);
 
         // Then: the first of the most frequent numbers is returned
-        Assertions.assertEquals(expectedMostCommon, mostCommon);
+        assertEquals(expectedMostCommon, mostCommon);
     }
 
     private List<Integer> toIntegerList(String inputNumbers) {
         String[] array = inputNumbers.split("\\s*,\\s*");
-        return Arrays.asList(array).stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        return Arrays.stream(array).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
     }
 }
