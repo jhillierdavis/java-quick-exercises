ae18a9f1e460499a72dfb486b3b22b1285ee8ad7
jhillierdavis <jhillierdavis@users.noreply.github.com>
2018-06-11 11:57:07 +0100

    Alternative implementation stub.

---
+++ src/main/java/com/jhdit/algorithms/frequency/MostFrequentNumberUsingStreams.java
@@ -0,0 +1,12 @@
+package com.jhdit.algorithms.frequency;
+
+import java.util.List;
+import java.util.Set;
+
+public class MostFrequentNumberUsingStreams implements MostFrequentNumber {
+
+    @Override
+    public Set<Integer> mostCommonNumber(List<Integer> numbers) {
+        throw new RuntimeException("TODO: Implement");
+    }
+}
