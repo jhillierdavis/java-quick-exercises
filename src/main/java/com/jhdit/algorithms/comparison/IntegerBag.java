514ea6a24b91d679fff9c2da74f1d52a8d4ccbba
jhillierdavis <jhillierdavis@users.noreply.github.com>
2018-06-20 15:07:23 +0100

    Added exercise (with additional unit test) for finding the largest terms of a sum.

--- src/main/java/com/jhdit/algorithms/comparison/IntegerBag.java
+++ src/main/java/com/jhdit/algorithms/comparison/IntegerBag.java
@@ -1,6 +1,7 @@
 package com.jhdit.algorithms.comparison;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,27 +9,54 @@ import java.util.List;
  */
 
 class IntegerBag {
-    private final List<Integer> numbers;
+    private final List<Integer> numbersInReverseOrder;
 
     IntegerBag(List<Integer> numbers) {
-        this.numbers = numbers;
+        Collections.sort(numbers, Collections.reverseOrder());
+        this.numbersInReverseOrder = numbers;
     }
 
     /**
      * Find all integers less than those in the list provided.
-     * @param numbersToCompare List of numbers to compare against
+     * @param numbersToCompare List of numbersInReverseOrder to compare against
      * @return List of integers in the bag that are less that the provided list.
      */
 
     List<Integer> findAllSmallerThan(List<Integer> numbersToCompare) {
+        Collections.sort(numbersToCompare); // Sort smallest to largest (for comparison efficiency)
+
         List<Integer> results = new ArrayList<>();
 
-        for (Integer entry: numbers) {
+        Collections.sort(numbersToCompare);
+
+        for (Integer entry: numbersInReverseOrder) {
             if (isLessThan(entry, numbersToCompare)) {
                 results.add(entry);
             }
         }
         return results;
+    }
+
+    List<Integer> findLargestTerms(Integer targetSum) {
+        if (targetSum < 0)    {
+            throw new IllegalArgumentException("Invalid negative sum value");
+        }
+
+        List<Integer> termsList = new ArrayList<>();
+        int sum = 0;
+        for (Integer number: this.numbersInReverseOrder)  {
+            if (sum >= targetSum) {
+                break;
+            }
+            sum += number;
+            termsList.add(number);
+        }
+
+        if (sum < targetSum) {
+            return Collections.emptyList();
+        }
+
+        return termsList;
     }
 
     private boolean isLessThan(Integer entry, List<Integer> numbersToCompare) {
--- src/test/java/com/jhdit/algorithms/comparison/IntegerBagTest.java
+++ src/test/java/com/jhdit/algorithms/comparison/IntegerBagTest.java
@@ -31,4 +31,26 @@ class IntegerBagTest {
         assertEquals(lessThanList.size(), expectedList.size(), "List size mismatch, expected: " + expectedList.size() + " , actual: " + lessThanList.size() );
         assertTrue(lessThanList.containsAll(expectedList), "List content mismatch");
     }
+
+    /**
+     * Finds lowest amount of numbers (from a list) which sum to over 100
+     */
+
+    @Test
+    void findLargestTerms()  {
+        // Setup
+        List<Integer> expectedList = Arrays.asList(46,32,32);
+
+        // Given: an instance of the SUT (Subject Under Test)
+        List<Integer> possibleTerms = Arrays.asList(5,6,3,23,46,4,32,3,32);
+        IntegerBag sut = new IntegerBag(possibleTerms);
+
+        // When: the SUT is invoked
+        List<Integer> largestTerms = sut.findLargestTerms(100);
+
+        // Then: the expected largest terms (addends, summands) are found
+        assertFalse( largestTerms.isEmpty() , "Unexpected empty results list" );
+        assertEquals(largestTerms, expectedList, "List size mismatch, expected: " + expectedList.size() + " , actual: " + largestTerms.size() );
+        assertTrue(expectedList.containsAll( largestTerms ), "List content mismatch");
+    }
 }
