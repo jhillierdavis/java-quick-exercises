5fa02ecbb46eca78e5c1a1cbaade06b16b9c4a36
jhillierdavis <jhillierdavis@users.noreply.github.com>
2018-06-22 10:58:54 +0100

    Refactored (extract method).

--- src/main/java/com/jhdit/concurrency/ParallelStreamBasedWorker.java
+++ src/main/java/com/jhdit/concurrency/ParallelStreamBasedWorker.java
@@ -6,17 +6,30 @@ public class ParallelStreamBasedWorker implements Worker {
     private Sender sender = new SlowSender();
 
     public int doWork(List<Integer> messages) {
-        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "10");
+        setThreadPoolSize(10);
 
-        int sum = messages.parallelStream().mapToInt(m -> {
-                // Handle unchecked exception
-                try {
-                    return this.sender.send(m);
-                } catch (InterruptedException ie) {
-                    throw new RuntimeException(ie);
-                }
-        }).sum();
+        int sum = calculateSumUsingStreams(messages);
 
         return sum;
+    }
+
+    /**r
+     * Override the default thread pool  size (set to the number of processors), used by parallel streams, to the supplied value.
+     * @param numberOfThreadsInPool Desired thread pool size
+     */
+
+    private void setThreadPoolSize(int numberOfThreadsInPool) {
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "" + numberOfThreadsInPool);
+    }
+
+    private int calculateSumUsingStreams(List<Integer> messages) {
+        return messages.parallelStream().mapToInt(m -> {
+                    // Handle unchecked exception
+                    try {
+                        return this.sender.send(m);
+                    } catch (InterruptedException ie) {
+                        throw new RuntimeException(ie);
+                    }
+            }).sum();
     }
 }
